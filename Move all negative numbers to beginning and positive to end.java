d552a39f10fdfbb367bc202ff8318651ae99cbde
Ashutosh Kumar Yadav <51047192+Ashus222@users.noreply.github.com>
2022-06-02 22:13:51 +0530

    Create Move all negative numbers to beginning and positive to end.java

---
+++ Move all negative numbers to beginning and positive to end.java
@@ -0,0 +1,24 @@
+public class Gfg {
+ 
+    // a utility function to swap two elements of an array
+    public static void swap(int[] ar, int i, int j)
+    {
+        int t = ar[i];
+        ar[i] = ar[j];
+        ar[j] = t;
+    }
+ 
+    // function to shilf all negative integers to the left
+    // and all positive integers to the right
+    // using Dutch National Flag Algorithm
+    public static void move(int[] ar)
+    {
+        int low = 0;
+        int high = ar.length - 1;
+        while (low <= high) {
+            if (ar[low] <= 0)
+                low++;
+            else
+                swap(ar, low, high--);
+        }
+    }
