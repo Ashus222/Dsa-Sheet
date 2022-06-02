b3b1bc4dfb70ae235dfa69e494f819c9774bf1f1
Ashutosh Kumar Yadav <51047192+Ashus222@users.noreply.github.com>
2022-06-02 22:22:17 +0530

    Update 6.Union of two arrays.java

--- 6.Union of two arrays.java
+++ 6.Union of two arrays.java
@@ -1,1 +1,19 @@
 
+class Solution{
+    public static int doUnion(int a[], int n, int b[], int m) 
+    {
+        
+        HashSet<Integer> set1 = new HashSet<>();
+
+       for(int i=0;i<n;i++){
+           set1.add(a[i]);
+       }
+
+       for(int i=0;i<m;i++){
+           set1.add(b[i]);
+       }
+
+
+       return set1.size();
+   }
+}
