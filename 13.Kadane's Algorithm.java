d9a893ec07d1343e7a5741db4ab0860664ba7c33
Ashutosh Kumar Yadav <51047192+Ashus222@users.noreply.github.com>
2022-06-03 12:56:49 +0530

    Create 13.Kadane's Algorithm.java

---
+++ 13.Kadane's Algorithm.java
@@ -0,0 +1,25 @@
+class Solution{
+
+    // arr: input array
+    // n: size of array
+    //Function to find the sum of contiguous subarray with maximum sum.
+    long maxSubarraySum(int arr[], int n){
+        
+        int sum=0, maxsum =Integer.MIN_VALUE;
+      
+      for(int i=0;i<n;i++){
+          sum+=arr[i];
+          if(sum>=maxsum){
+              maxsum=sum;
+          }
+          
+          if(sum<0){
+              sum=0;
+          }
+          
+      }
+      
+       return maxsum;
+   }
+}
+
